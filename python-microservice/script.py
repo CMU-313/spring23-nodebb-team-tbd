4262ba036cc19725b2714b6f4c8a824db7895692
sophiajwitt <sophiawitt33@gmail.com>
2023-04-05 18:01:23 -0400

    initialized dockerfile and python microservice

---
+++ python-microservice/script.py
@@ -0,0 +1,21 @@
+# import main Flask class 
+from flask import Flask
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    return 'Query String Example'
+
+@app.route('/form-example')
+def form_example():
+    return 'Form Data Example'
+
+@app.route('/json-example')
+def json_example():
+    return 'JSON Object Example'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 8000
+    app.run(debug=True, port=8000)
